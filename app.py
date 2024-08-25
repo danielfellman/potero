0b6457f5775bafcf2b254be59ca7b171df01a4a9
danielfellman <daniel.fellman@abo.fi>
2024-08-25 20:46:47 +0200

    fix

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ def portfolio():
     return render_template('portfolio.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
